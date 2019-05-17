a3cd41e2ff0d4fcdbb96bc63f5dbcdbdb2a48f55
7smd7 <32780546+7smd7@users.noreply.github.com>
2019-05-17 17:56:34 +0430

    edit mux

--- Mux 2-1.v
+++ Mux 2-1.v
@@ -1,9 +1,12 @@
 //MIPS
 module  mux_2_1(in0, in1, sel, out);
-  input  [15:0] in0;
-  input  [15:0] in1;
+  input  [31:0] in0;
+  input  [31:0] in1;
   input         sel;
 
-  output [15:0] out;
-  assign #2 out = (sel) ? in1 : in0;
+  output [31:0] out;
+  
+  always@(*) begin
+    assign out = (sel) ? in1 : in0;
+  end
 endmodule
