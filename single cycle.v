9dcdbcf042088a3b9526e52c2e0c2b64baa33ccf
7smd7 <32780546+7smd7@users.noreply.github.com>
2019-05-17 16:46:03 +0430

    mux

--- single cycle.v
+++ single cycle.v
@@ -6,3 +6,13 @@ module fulladder(a,b,c,s,ca);
     assign s=(a^b^c);
     assign ca=((a&b)|(b&c)|(c&a));
 endmodule 
+
+
+module  mux_2_1(in0, in1, sel, out);
+  input  [15:0] in0;
+  input  [15:0] in1;
+  input         sel;
+
+  output [15:0] out;
+  assign #2 out = (sel) ? in1 : in0;
+endmodule
